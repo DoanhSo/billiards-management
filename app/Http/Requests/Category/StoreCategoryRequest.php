f5871cb271e4acae1ff9394f9aaaf5e71c8344e6
DoanhSo <doanhfilwl@gmail.com>
2026-06-05 11:52:46 +0700

    [feat] Add StoreCategoryRequest - validate tao danh muc

---
+++ app/Http/Requests/Category/StoreCategoryRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests\Category;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreCategoryRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'name'        => ['required', 'string', 'max:255', 'unique:categories,name'],
+            'description' => ['nullable', 'string', 'max:500'],
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'name.required' => 'Vui lòng nhập tên danh mục.',
+            'name.unique'   => 'Tên danh mục đã tồn tại.',
+            'name.max'      => 'Tên danh mục không được vượt quá 255 ký tự.',
+        ];
+    }
+}
